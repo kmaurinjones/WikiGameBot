a41c78e40fccb070d61edf34ca0425b661a49d4d
KAI MAURIN-JONES <kmaurinj@student.ubc.ca>
2024-08-17 14:22:21 -0400

    ui updates

--- plots.py
+++ plots.py
@@ -33,12 +33,17 @@ def line_plot(game_csv):
                     mode='lines', 
                     line=dict(dash='dot', color='rgba(255, 105, 180, 0.8)', width=3))  # Complementary color with less opacity, thicker line
 
-    # Update the layout for the title, background colors, and hide the legend
+    # Update the layout for the title, background colors, and x-axis ticks
     fig.update_layout({
         'title': {
             'text': 'Similarity Over Game Turns',
             'x': 0.5,  # Centers the title
             'xanchor': 'center'
+        },
+        'xaxis': {
+            'tickmode': 'linear',
+            'dtick': 1,  # Ensure x-axis shows only integers
+            'title': 'Page Visited'
         },
         'showlegend': False  # Hide the legend
     })
